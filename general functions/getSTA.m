9329eb51a79458de0e11883caf4c05117c579358
ackrok <51799203+ackrok@users.noreply.github.com>
2026-04-13 13:43:31 -0400

    fixes

--- general functions/getSTA.m
+++ general functions/getSTA.m
@@ -24,13 +24,16 @@ function [sta, time, sta_z] = getSTA(signal, events, Fs, window)
 % Updated: Anya Krok, March 2020
 %
 
-range = [window(1)*Fs : window(2)*Fs]; nRange = length(range); % Window range, in samples, matching signal
+range = window(1)*Fs : window(2)*Fs; nRange = length(range); % Window range, in samples, matching signal
 time  = range(:)./Fs; % Time vector, in seconds
 
 sta = zeros(nRange, length(events)); % Initialize matrix with out
 mu = mean(signal); sigma = std(signal); % Compute mu, sigma over entire signal
 
 for x = 1:length(events)
+    if isnan(events(x))
+        sta(:,x) = nan(nRange, 1);
+    end
     eventFs = round(events(x).*Fs); % Event time matching sampling rate of singal
     if eventFs + range(1) >= 1 && eventFs + range(end) <= length(signal)
         sta(:,x) = signal(eventFs + range); % Extract signal from window around this event
